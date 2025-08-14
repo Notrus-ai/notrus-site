b7ead3ca2eafef49fb93da2e2c8634bf857a56d5
thiagodacororegio <thiagodacoregio@gmail.com>
2025-08-14 16:59:01 +0100

    Made the page benefits for navigation

---
+++ src/pages/Benefits.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Benefits ({t, notrusLogo}) {
+    return (
+        <div style={{padding: 40}}>
+            Página Benefícios
+        </div>
+    )
+}
+
+export default Benefits
